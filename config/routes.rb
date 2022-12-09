dafd7e14dd2df181bf3ad43669da5bd1ddf88f64
Charlotte Marion <marion.charlotte@orange.fr>
2022-12-09 17:31:53 +0100

    [Ajout] formulaire page inscription joueur

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   get '/inscription', to: 'pages#inscription'
   get '/inscription/recherche_joueur', to: 'pages#recherche_joueur'
-  get '/inscriptionForm', to: 'pages#inscription_form'
+  get '/inscription/new/formulaire', to: 'pages#inscription_form'
 
   root to: 'pages#inscription'
 
