b1c57671d2a7da873df0727542a3c192f9b5bf4b
Charlotte Marion <marion.charlotte@orange.fr>
2023-04-19 07:24:48 +0200

    [Factorisation] suppression du code mort

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
 
   def accueil
-    if !current_utilisateur.present?
-      redirect_to new_utilisateur_session_path
-    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :utilisateurs
   resources :utilisateurs, only: [:new, :create, :show]
 
-  get "/Baco/admin", to: "pages#accueil", as: "pages_accueil"
+  # get "/Baco/admin", to: "pages#accueil", as: "pages_accueil"
 
   root to: 'pages#accueil'
 
