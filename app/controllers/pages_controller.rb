59062b98c2c2f94b1b5b85f7f83d43a4f1924dba
Charlotte Marion <marion.charlotte@orange.fr>
2023-04-24 22:23:36 +0200

    [Modification] header pour intégrer dans la page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def accueil
     if current_utilisateur.admin?
       @utilisateurs = Utilisateur.all
+      @nombre_membres = @utilisateurs.count
     end
   end
 
