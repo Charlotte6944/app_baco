6f1484b5c44822007974923d5fd0f17c8c190c9a
Charlotte Marion <marion.charlotte@orange.fr>
2023-04-26 19:56:05 +0200

    [Modification] vérouillage accès à la page d'accueil aux admins

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,19 @@
 class PagesController < ApplicationController
 
+  before_action :verify_admin
+
   def accueil
-    if current_utilisateur.admin?
-      @utilisateurs = Utilisateur.all
-      @nombre_membres = @utilisateurs.count
+    @utilisateurs = Utilisateur.all
+    @nombre_membres = @utilisateurs.count
+  end
+
+  private
+
+  def verify_admin
+    unless current_utilisateur.admin?
+      flash[:alert] = "Vous n'avez pas les droits d'administrateur."
+      sign_out current_utilisateur
+      redirect_to new_utilisateur_session_path
     end
   end
 
